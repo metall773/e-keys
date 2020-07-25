3c586ef887c23b4650c700ab5e96d772302a8b23
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-26 00:15:37 +0200

    fix share script

--- scripts/windows-init.ps1
+++ scripts/windows-init.ps1
@@ -8,6 +8,15 @@ param(
   $share_pass
 )
 
+Function LogWrite
+{
+  Param ([string]$log1, [string]$log2, [string]$log3, [string]$log4,  [string]$log5)
+  $stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+  $line = "$stamp $log1 $log2 $log3 $log4 $log5"
+  Write-host $line
+  Add-content $logFile -value $Line
+}
+
 #format RAW disks
 Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | `
   Select-Object -First 1 | `
@@ -32,16 +41,15 @@ foreach ($disk in $disks) {
 
 #create c:\mount_share.cmd
 $share_file = @"
-cmdkey /add:"$share_host" /user:"Azure\$share_login" /pass:"$share_pass"
-net use y: /delete /y
-net use y: \\$share_host\$share_name
-pause
+cmdkey /add:"$share_host" /user:"Azure\$share_login" /pass:"$share_pass" ;
+net use y: /delete /y ;
+net use y: \\$share_host\$share_name 
 "@
 
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllLines('c:\mount_share.cmd', $share_file, $Utf8NoBomEncoding)
 
 #rename network disk
-c:\mount_share.cmd
-$Rename = New-Object -ComObject Shell.Application
-$Rename.NameSpace("Y:\").Self.Name = $share_name
+#c:\mount_share.cmd
+#$Rename = New-Object -ComObject Shell.Application
+#$Rename.NameSpace("Y:\").Self.Name = $share_name
