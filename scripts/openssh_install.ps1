d462b25b2acca968f0a7699b8c5b91f4ffc44bba
metall773 <64757081+metall773@users.noreply.github.com>
2021-01-01 18:46:35 +0100

    Update openssh_install.ps1

--- scripts/openssh_install.ps1
+++ scripts/openssh_install.ps1
@@ -47,3 +47,27 @@ $systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYS
 $acl.SetAccessRule($administratorsRule)
 $acl.SetAccessRule($systemRule)
 $acl | Set-Acl
+
+
+#format RAW disks
+Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | `
+  Select-Object -First 1 | `
+    Set-WmiInstance -Arguments @{DriveLetter='Z:'}
+
+$disks = Get-Disk | `
+  Where-Object partitionstyle -eq 'raw' | `
+    Sort-Object number
+
+$letters = 69..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = 'datadisk'
+
+foreach ($disk in $disks) {
+  $driveLetter = $letters[$count].ToString()
+  $diskLabel = -join ($label,'.', $count)
+  LogWrite " Format disk " $driveLetter $diskLabel
+  $disk | Initialize-Disk -PartitionStyle MBR -PassThru | `
+      New-Partition -UseMaximumSize -DriveLetter $driveLetter | `
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $diskLabel -Confirm:$false -Force
+  $count++
+}
